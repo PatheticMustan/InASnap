ddd1720ecd83d94e172a655e55bed37f3a11f18b
RedWool <leolui0930.sia@gmail.com>
2023-01-07 11:04:51 -0500

    Input Stuff

---
+++ Assets/Scripts/InputReciever.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReciever : MonoBehaviour
+{
+    public InputMaster controls;
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
