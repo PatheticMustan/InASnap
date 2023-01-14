8838c6595b0c7cc142828da16f43efec2dde48d2
RedWool <leolui0930.sia@gmail.com>
2023-01-14 10:57:59 -0500

    Input Modify, WIP
    the input system but part of it, not all

--- Assets/Scripts/InputReciever.cs
+++ Assets/Scripts/InputReciever.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class InputReciever : MonoBehaviour
 {
-    public InputMaster controls;
+    protected InputMaster controls;
+
+    private void Awake()
+    {
+        controls.Player.APress.performed += _ => APress();
+        controls.Player.BPress.performed += _ => BPress();
+        controls.Player.Direction.performed += dir => Direction(dir.ReadValue<Vector2>());
+    }
+
+    protected virtual void APress()
+    {
+
+    }
+
+    protected virtual void BPress()
+    {
+
+    }
+
+    protected virtual void Direction(Vector2 dir)
+    {
+
+    }
 
     private void OnEnable()
     {
