04de176bffd1b488f176d2b5702dcb719c2f1562
Mustan Pathetic <patheticmustan@gmail.com>
2023-01-14 17:07:13 -0500

    incomplete combo system

--- Assets/Scripts/PlayerInput/ComboDictionary.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/Combo Dictionary")]
-public class ComboDictionary : ScriptableObject
-{
-    //Put varible for the combo in here as a array, so we can put as many combos as we want in here and in seperate combo dictionaries for each level
-}
