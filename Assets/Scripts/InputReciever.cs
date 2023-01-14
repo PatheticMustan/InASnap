0dc5c756e65ec1482f47443e65b9224d3dfdc9b7
RedWool <leolui0930.sia@gmail.com>
2023-01-14 12:22:29 -0500

    Broken Input systen
    idk why I can't add the right object

--- Assets/Scripts/InputReciever.cs
+++ Assets/Scripts/InputReciever.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputReciever : MonoBehaviour
 {
-    protected InputControls controls;
+    [SerializeField] protected InputController controls;
+    //[SerializeField] protected ComboDictionary combos;
+
+    public Queue<InputID> inputQueue { protected get; set; }
+    protected float timeBetweenActions;
 
     private void Awake()
     {
@@ -13,19 +18,24 @@ public class InputReciever : MonoBehaviour
         controls.Player.Direction.performed += dir => Direction(dir.ReadValue<Vector2>());
     }
 
+    protected virtual void InputAction(InputID inputID)
+    {
+        Debug.Log(inputID);
+    }
+
     protected virtual void APress()
     {
-
+        InputAction(InputID.A);
     }
 
     protected virtual void BPress()
     {
-
+        InputAction(InputID.B);
     }
 
     protected virtual void Direction(Vector2 dir)
     {
-
+        InputAction((InputID)(Mathf.Round(Mathf.Atan2(dir.y, dir.x) / 0.7853f) + 1));
     }
 
     private void OnEnable()
@@ -37,4 +47,19 @@ public class InputReciever : MonoBehaviour
     {
         controls.Disable();
     }
+}
+
+public enum InputID
+{
+    N = 0,
+    L = 1,
+    LU = 2,
+    U = 3,
+    RU = 4,
+    R = 5,
+    RD = 6,
+    D = 7,
+    LD = 8,
+    A = 9,
+    B = 10
 }
