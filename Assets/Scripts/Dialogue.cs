fd550355c9b9e57d33e7f2cb221961e3a66fe17b
Sama <samaaeldijwy.sia@gmail.com>
2023-01-28 18:24:39 +0200

    Auto stash before merge of "RedWoolTest" and "origin/RedWoolTest"

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue  : MonoBehaviour
+{
+    public TextMeshProUGUI textcomponent;
+    public string[] lines;
+    public float textspeed;
+    private int index;
+
+
+    void Start()
+    {
+        textcomponent.text = string.Empty;
+        StartD();
+    }
+    private void StartD()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textcomponent.text += c;
+            yield return new WaitForSeconds(textspeed);
+        }
+    }
+
+
+}
---
+++ Assets/StartPage.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPage : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("exited");
+    }
+
+
+    public void StartLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
