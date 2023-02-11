3d66b9e20815a95475d00712b2dfa395ae3b14a4
Mustan Pathetic <patheticmustan@gmail.com>
2023-02-11 15:47:43 -0500

    fixed editor build bug

---
+++ Assets/Editor/LevelEditor/ComboDictionaryInspector.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+#if (UNITY_EDITOR)
+[CustomEditor(typeof(ComboSystem))]
+public class ComboDictionaryInspector : Editor {
+    
+}
+#endif
